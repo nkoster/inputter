744563097b7b24690111f05cb34820334fd06c9e
Niels Koster <niels.koster@portavita.nl>
2021-03-03 16:16:14 +0100

    Add a bit of fetch logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+import { TextField } from '@material-ui/core'
 
 function App() {
+
+  const [query, setQuery] = useState('')
+  const [data, setData] = useState([])
+
+  const onChange = async evt => setQuery(evt.target.value)
+
+  useEffect(async _ => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${query}`)
+      .then(res => res.json())
+      console.log('RESPONSE',response)
+    setData(response)
+  }, [query])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <TextField
+        onChange={onChange}
+        value={query}
+        color='primary'
+        type='search'
+        label='search'
+        placeholder='...'
+      /> {/* text color in App.css: input */}
+      <ul>
+        {Array.isArray(data) ? data.map(d => <li key={d.title}>{d.title}</li>) : data.title ? <li>{data.title}</li> : <li>nothing here...</li>}
+        </ul>
       </header>
     </div>
-  );
+  )
+
 }
 
-export default App;
+export default App
