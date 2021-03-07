09a6cb61623a47c83e24cf93a2a35d21bf8dfe34
Niels Koster <niels.koster@portavita.nl>
2021-03-07 21:54:58 +0100

    not okay yet but I need the commit

--- src/components/Lister.js
+++ src/components/Lister.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import { DataGrid } from '@material-ui/data-grid'
 
+const columns = [
+  { field: 'kafka_topic', headerName: 'Kafka Topic', width: '200px' },
+  { field: 'kafka_offset', headerName: 'Kafka Offset', width: '200px' },
+  { field: 'identifier_type', headerName: 'Type', width: '200px' },
+  { field: 'identifier_value', headerName: 'Value', width: '200px' }
+]
+
 const Lister = ({data}) => {
-    return (
-        <div className='list'>
-        {Array.isArray(data) ? data.map(d => 
-          <div key={d.identifier_value}>{d.identifier_value}</div>
-        ) : data ? <div>{data}</div> : <div>nothing here...</div>}
-      </div>
-    )
+  return (
+    // <div style={{ height: 500, width: '100%' }}>
+    //   <DataGrid hideFooter rows={data} columns={columns} />
+    // </div>
+    <div className='list'>
+      {Array.isArray(data) ? data.map(d => 
+        <div key={d.identifier_value}>{d.identifier_value}</div>
+      ) : data ? <div>{data}</div> : <div>nothing here...</div>}
+    </div>
+  )
 }
+
 export default Lister
