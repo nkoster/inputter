f9881f739c2279ff3facce7d67b2089ed0e4835b
Niels Koster <niels.koster@portavita.nl>
2021-03-08 12:44:03 +0100

    DataGrid layout + show only when data available

--- src/components/Lister.js
+++ src/components/Lister.js
@@ -2,26 +2,19 @@ import React from 'react'
 import { DataGrid } from '@material-ui/data-grid'
 
 const columns = [
-  { field: 'kafka_topic', headerName: 'Kafka Topic', width: '200px' },
-  { field: 'kafka_offset', headerName: 'Kafka Offset', width: '200px' },
-  { field: 'identifier_type', headerName: 'Type', width: '200px' },
-  { field: 'identifier_value', headerName: 'Value', width: '200px' },
-  { field: 'id' }
+  { field: 'kafka_topic', headerName: 'Kafka Topic', flex: 1 },
+  { field: 'kafka_offset', headerName: 'Kafka Offset', width: 200 },
+  { field: 'identifier_type', headerName: 'Type', width: 200 },
+  { field: 'identifier_value', headerName: 'Value', width: 300 },
 ]
 
 const Lister = ({data}) => {
   return (
     <div style={{ height: 500, width: '100%' }}>
-      <DataGrid hideFooter rows={data.map(d => {
-        console.log(d)
+      {data.length > 0 && <DataGrid hideFooter rows={data.map(d => {
         return { ...d, id: Math.random().toString(20)}
-      })} columns={columns} />
+      })} columns={columns} />}
     </div>
-    // <div className='list'>
-    //   {Array.isArray(data) ? data.map(d => 
-    //     <div key={d.identifier_value}>{d.identifier_value}</div>
-    //   ) : data ? <div>{data}</div> : <div>nothing here...</div>}
-    // </div>
   )
 }
 
