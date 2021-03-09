49ee6dcaab5e208893d3549f0e99a42964b0547d
Niels Koster <niels.koster@portavita.nl>
2021-03-09 13:19:19 +0100

    Show progress while loading

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
-import { TextField, ThemeProvider, createMuiTheme } from '@material-ui/core'
+import { TextField, ThemeProvider, createMuiTheme, CircularProgress } from '@material-ui/core'
 import { green, orange } from '@material-ui/core/colors'
 import axios from 'axios'
 import Lister from './components/Lister'
@@ -21,47 +21,55 @@ const App = _ => {
 
   const [query, setQuery] = useState('')
   const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
   const img = useRef()
   const onChange = evt => {
     setQuery(evt.target.value)
     evt.target.value || setData([])
   }
 
-  useEffect(async _ => {
-    if (query) {
-      try {
-        await axios.post('http://localhost:3334/api/v1/search/', {
-          search: query
-        }, {
-          mode: 'no-cors'
-        })
-        .then(res => {
-          setData(res.data)
-        })        
-      } catch(err) {
-        console.warn(err.message)
+  useEffect(_ => {
+    const timeout = setTimeout(async _ => {
+      if (query) {
+        try {
+          setLoading(true)
+          await axios.post('http://localhost:3334/api/v1/search/', {
+            search: query
+          }, {
+            mode: 'no-cors'
+          })
+          .then(res => {
+            setData(res.data)
+            setLoading(false)
+          })
+        } catch(err) {
+          console.warn(err.message)
+          setData([])
+          setLoading(false)
+        }
+      } else {
         setData([])
       }
-    } else {
-      setData([])
-    }
+    }, 250)
+    return _ => clearTimeout(timeout)
   }, [query])
-
+  
   useEffect(_ => {
    setTimeout(_ => {
      img.current.style.height = '100px'
-     setTimeout(_ => img.current.style.height = '0', 2000)
-   }, 2000)
+     setTimeout(_ => img.current.style.height = '0', 1000)
+   }, 1000)
   }, [])
 
   return (
     <div className="App">
       <header className="App-header">
+      <ThemeProvider theme={theme}>
         <div style={{ width: '100%' }}>
           <div style={{ display: 'inline-block' }}>
             <img ref={img} style={imgStyle} src={fhirDepartment} alt='FHIR department' />
           </div>
-          <ThemeProvider theme={theme}>
             <TextField
               style={{ width: '80%', margin: 20 }}
               margin='dense'
@@ -73,9 +81,9 @@ const App = _ => {
               label='search'
               placeholder='...'
             /> {/* text color in App.css: input */}
-          </ThemeProvider>
-        </div>
-        <Lister data={data} />
+          </div>
+          {loading ? <CircularProgress color='primary' /> : <Lister data={data} />}
+        </ThemeProvider>
       </header>
     </div>
   )
