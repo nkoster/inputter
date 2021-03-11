9ecf4e8e8854dd395aa327aac9ecab768605a7db
Niels Koster <n.koster@portavita.eu>
2021-03-11 22:58:17 +0100

    Better screen layout

--- src/components/Lister.js
+++ src/components/Lister.js
@@ -4,13 +4,13 @@ import { DataGrid } from '@material-ui/data-grid'
 const columns = [
   { field: 'kafka_topic', headerName: 'Kafka Topic', flex: 1 },
   { field: 'kafka_offset', headerName: 'Kafka Offset', flex: 0.4 },
-  { field: 'identifier_type', headerName: 'Identifier Type', flex: 1 },
+  { field: 'identifier_type', headerName: 'Identifier Type', flex: 0.6 },
   { field: 'identifier_value', headerName: 'Identifier Value', flex: 1 },
 ]
 
 const Lister = ({data, limit}) => {
   return (
-    <div style={{ height: 500, width: '100%' }}>
+    <div style={{ height: '82vh', width: '100%' }}>
       {(data.length > 0 && data.length < limit) && <DataGrid hideFooter rows={data.map(d => {
         return { ...d, id: Math.random().toString(20)}
       })} columns={columns} />}
