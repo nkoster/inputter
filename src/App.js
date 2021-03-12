5536d63321416dba562f6ef92a2f773bb1cdcc5d
Niels Koster <niels.koster@portavita.nl>
2021-03-12 12:58:44 +0100

    Add kafka topic query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useState, useEffect, useRef } from 'react'
 import './App.css'
-import { TextField, ThemeProvider, createMuiTheme, CircularProgress } from '@material-ui/core'
+import { TextField, ThemeProvider, createMuiTheme } from '@material-ui/core'
 import { green, orange } from '@material-ui/core/colors'
 import axios from 'axios'
 import Lister from './components/Lister'
-// import fhirDepartment from '../src/pix/lego-fhir-station-2.png'
 import fhirDepartment1 from '../src/pix/fire1.png'
 import fhirDepartment2 from '../src/pix/fire2.png'
 import { ScaleLoader } from 'react-spinners'
@@ -17,7 +16,7 @@ const theme = createMuiTheme({
       main: orange[800],
     },
     secondary: {
-      main: orange[800]
+      main: green[800]
     }
   }
 })
@@ -26,6 +25,7 @@ const App = _ => {
 
   const [queryIdentifierValue, setQueryIdentifierValue] = useState('')
   const [queryKafkaOffset, setQueryKafkaOffset] = useState('')
+  const [queryKafkaTopic, setQueryKafkaTopic] = useState('')
   const [data, setData] = useState([])
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
@@ -36,24 +36,28 @@ const App = _ => {
   const onChangeIdentifierValue = evt => {
     setQueryIdentifierValue(evt.target.value)
     setError('')
-    // evt.target.value || setData([])
   }
 
   const onChangeKafkaOffset = evt => {
     setQueryKafkaOffset(evt.target.value)
     setError('')
-    // evt.target.value || setData([])
+  }
+
+  const onChangeKafkaTopic = evt => {
+    setQueryKafkaTopic(evt.target.value)
+    setError('')
   }
 
   useEffect(_ => {
     const timeout = setTimeout(async _ => {
       localStorage.setItem('queryIdentifierValue', queryIdentifierValue ? queryIdentifierValue : '')
       localStorage.setItem('queryKafkaOffset', queryKafkaOffset ? queryKafkaOffset : '')
-      if (queryIdentifierValue || queryKafkaOffset) {
+      localStorage.setItem('queryKafkaTopic', queryKafkaTopic ? queryKafkaTopic : '')
+      if (queryIdentifierValue || queryKafkaOffset || queryKafkaTopic) {
         try {
           setLoading(true)
           await axios.post('http://localhost:3333/api/v1/search/', {
-            search: { queryIdentifierValue, queryKafkaOffset }
+            search: { queryIdentifierValue, queryKafkaOffset, queryKafkaTopic }
           }, {
             mode: 'no-cors'
           })
@@ -72,16 +76,15 @@ const App = _ => {
       }
     }, 250)
     return _ => clearTimeout(timeout)
-  }, [queryIdentifierValue, queryKafkaOffset])
+  }, [queryIdentifierValue, queryKafkaOffset, queryKafkaTopic])
   
   useEffect(_ => {
     setQueryIdentifierValue(localStorage.getItem('queryIdentifierValue'))
     setQueryKafkaOffset(localStorage.getItem('queryKafkaOffset'))
+    setQueryKafkaTopic(localStorage.getItem('queryKafkaTopic'))
     setTimeout(_ => {
       img1.current.style.height = '0px'
       setTimeout(_ => img1.current.style.display = 'none', 1005)
-      // if (img2.current) img2.current.style.opacity = '1'
-      // setTimeout(_ => img.current.style.height = '0px', 2000)
     }, 1000)
   }, [])
 
@@ -98,11 +101,11 @@ const App = _ => {
                 style={{ flex: '1', margin: 10 }}
                 margin='dense'
                 variant='standard'
-                onChange={onChangeIdentifierValue}
-                value={queryIdentifierValue}
+                onChange={onChangeKafkaTopic}
+                value={queryKafkaTopic}
                 color='primary'
                 type='search'
-                label='Search Identifier Value'
+                label='Search Kafka Topic'
                 placeholder='...'
               /> {/* text color in App.css: input */}
               <TextField
@@ -116,6 +119,17 @@ const App = _ => {
                 label='Search Kafka Offset'
                 placeholder='...'
               /> {/* text color in App.css: input */}
+              <TextField
+                style={{ flex: '1', margin: 10 }}
+                margin='dense'
+                variant='standard'
+                onChange={onChangeIdentifierValue}
+                value={queryIdentifierValue}
+                color='primary'
+                type='search'
+                label='Search Identifier Value'
+                placeholder='...'
+              /> {/* text color in App.css: input */}
             </div>
             {data.length > 0 && <div style={{ display: 'inline-block', paddingTop: '20px', fontSize: '14px' }}>
               <br />{data.length > 50 ? '50+' : data.length}<br />row{data.length === 1 ? '' : 's'}
@@ -123,8 +137,8 @@ const App = _ => {
             <div></div>
             {loading ? <ScaleLoader color='orange'/> : (data.length > 0 && <Lister data={data} limit={LIMIT} />)}
             {error && <p style={{ fontSize: '18px', color: 'black' }}>{error}</p>}
-            {data.length === 0 && !loading && !error && (queryIdentifierValue || queryKafkaOffset) ? <p style={{ fontSize: '18px', color: 'black' }}>No records found</p> : null}
-            {(!queryIdentifierValue && !queryKafkaOffset) && <div><img ref={img2} style={fire2Style} src={fhirDepartment2} alt='FHIR Station' /></div>}
+            {data.length === 0 && !loading && !error && (queryIdentifierValue || queryKafkaOffset || queryKafkaTopic) ? <p style={{ fontSize: '18px', color: 'black' }}>No records found</p> : null}
+            {(!queryIdentifierValue && !queryKafkaOffset && !queryKafkaTopic) && <div><img ref={img2} style={fire2Style} src={fhirDepartment2} alt='FHIR Station' /></div>}
           </div>
         </ThemeProvider>
       </header>
