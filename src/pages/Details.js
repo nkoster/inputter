20e4079dbd433308df23292f5cd7636241889222
Niels Koster <n.koster@portavita.eu>
2021-03-17 22:47:17 +0100

    Placeholder for kafka details

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
-const Details = _ => {
+const Details = ({ match }) => {
+  const { topic, offset } = match.params
   return (
-    <>
-      Details
-    </>
+    <div style={{display: 'flex', justifyContent: 'center', height: '94vh'}}>
+      <div style={{display: 'flex', alignItems: 'center'}}>
+        <div>
+          Jetzt geht's Los!
+          <h2>{topic}</h2>
+          <h2>{offset}</h2>
+        </div>
+      </div>
+    </div>
   )
 }
 
