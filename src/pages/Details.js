57a1cb3d2725ec31842d39047781d4bfb3af59f1
Niels Koster <n.koster@portavita.eu>
2021-03-17 23:41:50 +0100

    Pass stuff that is not in the URL, via state object

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,7 +1,9 @@
 import React from 'react'
 
-const Details = ({ match }) => {
-  const { topic, offset } = match.params
+const Details = props => {
+  const { topic, offset } = props.match.params
+  const { identifierType } = props.location.state || {}
+  const { identifierValue } = props.location.state || {}
   return (
     <div style={{display: 'flex', justifyContent: 'center', height: '94vh'}}>
       <div style={{display: 'flex', alignItems: 'center'}}>
@@ -9,6 +11,8 @@ const Details = ({ match }) => {
           Jetzt geht's Los!
           <h2>{topic}</h2>
           <h2>{offset}</h2>
+          <h2>{identifierType}</h2>
+          <h2>{identifierValue}</h2>
         </div>
       </div>
     </div>
