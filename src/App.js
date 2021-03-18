07305aee2b222a8a46c855e6f889da31b9214bdb
Niels Koster <n.koster@portavita.eu>
2021-03-18 01:28:45 +0100

    Move router to Home

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import './App.css'
 import Home from './pages/Home'
-import Details from './pages/Details'
-import { Route, Switch } from 'react-router-dom'
 
 const App = _ => {
 
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/details/:topic/:offset" component={Details} />
-    </Switch>
+      <Home />
   )
 
 }
