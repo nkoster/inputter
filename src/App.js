b4c85eff09f273a34ce7a39b91a4646b0ed4a352
Niels Koster <niels.koster@portavita.nl>
2021-03-18 14:12:53 +0100

    Splash in App, not in Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css'
 import Home from './pages/Home'
+import { useRef, useEffect, useState } from 'react'
+import fhirDepartment1 from '/home/niels/src/js/react/inputter/src/pix/fire1.png'
 
 const App = _ => {
+  const [loading, setLoading] = useState(true)
+  const img1 = useRef()
+  useEffect(_ => {
+    setTimeout(_ => {
+      img1.current.style.height = '0px'
+      setTimeout(_ => {
+        img1.current.style.display = 'none'
+        setLoading(false)
+    }, 700)
+    }, 500)
+  }, [])
   return (
-      <Home />
+    <>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img ref={img1} style={fire1Style} src={fhirDepartment1} alt='FHIR department' />
+      </div>
+      {loading ? null : <Home />}
+    </>
   )
+}
+
+const fire1Style = {
+  padding: 0, margin: 0,
+  height: 300,
+  transition: 'height 0.7s'
 }
 
 export default App
