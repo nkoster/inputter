3bab97268082c4abf816d7fcc8a0b298e03ca9ad
Niels Koster <niels.koster@portavita.nl>
2021-03-22 16:29:34 +0100

    Bug: useHistory stuff needs to be inside the exported react function

--- src/components/Lister.js
+++ src/components/Lister.js
@@ -2,33 +2,36 @@ import React from 'react'
 import { DataGrid } from '@material-ui/data-grid'
 import { useHistory } from 'react-router-dom'
 
-const renderCell = data => {
-  const history = useHistory()
-  const onClick = _ => {
-    history.push({
-      pathname: `/details/${data.row.kafka_topic}/${data.row.kafka_offset}`,
-      state: {
-        identifierType: data.row.identifier_type,
-        identifierValue: data.row.identifier_value
-      }
-    })
-  }
-  return (
-    <div
-      onClick={onClick}
-      style={{cursor: 'pointer'}}
-    >{data.row.kafka_offset}</div>
-  )
-}
-
-const columns = [
-  { field: 'kafka_topic', headerName: 'Kafka Topic', flex: 1 },
-  { field: 'kafka_offset', headerName: 'Kafka Offset', flex: 0.4, renderCell },
-  { field: 'identifier_type', headerName: 'Identifier Type', flex: 0.6 },
-  { field: 'identifier_value', headerName: 'Identifier Value', flex: 1 },
-]
-
 const Lister = ({data, limit}) => {
+
+  const history = useHistory()
+  const renderCell = data => {
+  
+    const onClick = _ => {
+      history.push({
+        pathname: `/details/${data.row.kafka_topic}/${data.row.kafka_offset}`,
+        state: {
+          identifierType: data.row.identifier_type,
+          identifierValue: data.row.identifier_value
+        }
+      })
+    }
+
+    return (
+      <div
+        onClick={onClick}
+        style={{cursor: 'pointer'}}
+      >{data.row.kafka_offset}</div>
+    )
+  }
+  
+  const columns = [
+    { field: 'kafka_topic', headerName: 'Kafka Topic', flex: 1 },
+    { field: 'kafka_offset', headerName: 'Kafka Offset', flex: 0.4, renderCell },
+    { field: 'identifier_type', headerName: 'Identifier Type', flex: 0.6 },
+    { field: 'identifier_value', headerName: 'Identifier Value', flex: 1 },
+  ]
+
   return (
     <div style={{ height: '82vh', width: '100%' }}>
       {(data.length > 0 && data.length < limit) && <DataGrid hideFooter rows={data.map(d => {
